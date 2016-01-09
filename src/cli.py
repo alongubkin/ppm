78e43eb644565d8afbb8e201fa1a1c2d5817fb02
Alon Gubkin <alon.gubkin@gmail.com>
2016-01-09 18:27:56 +0200

    run command

--- src/cli.py
+++ src/cli.py
@@ -1,16 +1,21 @@
 import os
 import click
 
+import json
+import subprocess
+
+from docker import Client
+from docker.tls import TLSConfig
+import dockerpty
+
 from package import Package
 
 PACKAGE_FILE_NAME = "ppm-package.json"
+MOUNT_DIRECTORY = "/mnt/app"
 
 @click.group()
 def cli():
     pass
-
-def test():
-    return "abb"
 
 @click.command()
 @click.option("--name", prompt="Name",
@@ -39,8 +44,47 @@ def init(name, version, description, entry_point):
     else:
         click.echo(click.style("Aborted.", fg="red", bold=True))
 
+@click.command()
+def run():
+    package_file_path = os.path.join(os.getcwd(), PACKAGE_FILE_NAME)
+    with open(package_file_path, "r") as package_file:
+        package = Package.deserialize(package_file.read())
+
+    config = json.loads(subprocess.check_output(["docker-machine", "inspect", "dev"]))
+
+    if not config["HostOptions"]["EngineOptions"]["TlsVerify"]:
+        raise Exception("TLS is required.")
+
+    base_url = "https://{}:2376".format(config["Driver"]["IPAddress"])
+
+    cert_path = config["HostOptions"]["AuthOptions"]["StorePath"]
+
+    tls_config = TLSConfig(client_cert=(os.path.join(cert_path, 'cert.pem'),
+                                        os.path.join(cert_path, 'key.pem')),
+                           ca_cert=os.path.join(cert_path, 'ca.pem'),
+                           verify=True,
+                           assert_hostname=False)
+
+    client = Client(base_url=base_url, tls=tls_config)
+
+    container = client.create_container(
+        image="library/python:3-onbuild",
+        stdin_open=True,
+        tty=True,
+        command="python {}".format(package.entry_point),
+        working_dir=MOUNT_DIRECTORY,
+        host_config=client.create_host_config(binds={
+            os.getcwd(): {
+                "bind": MOUNT_DIRECTORY,
+                "mode": "rw"
+            }
+        })
+    )
+
+    dockerpty.start(client, container)
 
 cli.add_command(init)
+cli.add_command(run)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
--- src/package.py
+++ src/package.py
@@ -7,6 +7,10 @@ class Package(object):
         self._description = description
         self._entry_point = entry_point
 
+    @property
+    def entry_point(self):
+        return self._entry_point
+    
     def serialize(self):
         return json.dumps({
             "name": self._name,
@@ -14,3 +18,11 @@ class Package(object):
             "description": self._description,
             "entry_point": self._entry_point
         }, indent=4, sort_keys=True)
+
+    @staticmethod
+    def deserialize(data):
+        parsed_data = json.loads(data)
+        return Package(name=parsed_data.get("name"),
+                       version=parsed_data.get("version"),
+                       description=parsed_data.get("description", None),
+                       entry_point=parsed_data.get("entry_point", None))
